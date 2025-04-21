65f80b33bd559e0211fe9f0c9398468762ec13a5
“AanikethG” <aanikethghosh@gmail.com>
2025-04-21 13:29:29 +0530

    Modified process_application.php

--- backend/process_application.php
+++ backend/process_application.php
@@ -4,17 +4,8 @@
 // process_application.php
 
 
-// 1. Database Connection (Replace with your actual credentials)
-$servername = "your_db_host";
-$username = "your_db_user";
-$password = "your_db_password";
-$dbname = "your_db_name";
-
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-}
+// 1. Database Connection
+include 'db_connect.php';
 
 
 // 2. Get Program Type
